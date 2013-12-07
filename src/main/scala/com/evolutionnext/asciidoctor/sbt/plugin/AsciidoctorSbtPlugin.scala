6dc3f3bb71c005156ef791ec5ec3e71e48f9986c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-12-07 10:09:12 -0700

    Added Plugin SettingKeys

--- src/main/scala/com/evolutionnext/asciidoctor/sbt/plugin/AsciidoctorSbtPlugin.scala
+++ src/main/scala/com/evolutionnext/asciidoctor/sbt/plugin/AsciidoctorSbtPlugin.scala
@@ -1,5 +1,7 @@
 package com.evolutionnext.asciidoctor.sbt.plugin
 
+import sbt._
+
 /**
  * Copyright 2013 Daniel Hinojosa and Daniel Allen
  *
@@ -15,7 +17,48 @@ package com.evolutionnext.asciidoctor.sbt.plugin
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-object AsciidoctorSbtPlugin {
-  
+object AsciidoctorSbtPlugin extends Plugin {
+  val asciidoctorTask = TaskKey[Unit]("asciidoctor-task")
+  val newSetting = SettingKey[String]("A new setting.")
 
+  /*
+   * sourceDir where the asciidoctor .asc, .asciidoc files are located, default is `src/asciidoc`
+   */
+  val asciidoctorSources = SettingKey[File](
+    """Location of where the asciidoc sources with .asc,
+      | .asciidoctor, .adoc, or .ad extensions (Default: src/asciidoctor)""".stripMargin)
+
+  /**
+   * sourceDocumentName
+   * an override to process a single source file. Type: File. Defaults to all files in asciidoctorSources
+   */
+  val asciidoctorSingleSourceDocumentName = SettingKey[File](
+    """Location of a single .asc, .asciidoctor, .adoc, or .ad file.
+       | Type: File. Defaults to all files in %s""".format(asciidoctorSources).stripMargin)
+
+
+  /**
+   * outputDir where generated docs go. Type: File. Default: target/asciidoctor
+   */
+  val asciidoctorOutputDir = SettingKey[File]("Location where the rendered documents will be stored (Default: target/asciidoctor)")
+
+  /*
+   * backend the backend to use. Type: String. Default: html5. This option is a convenience for using backends property.
+   */
+  val asciidoctorSingleBackend = SettingKey[String]("Single backends to process asciidoctor files (Default: html5)")
+
+  /*
+   * backends the backends to use. Type: Set<String>. Default: [html5]
+   */
+  val asciidoctorBackends = SettingKey[Set[String]]("Multiple Backends used to process asciidoctor files (Default: [html5]")
+
+  /*
+    options a Map specifying different options that can be sent to Asciidoctor.
+  */
+  val asciidoctorOptions = SettingKey[Map[String, String]]("Options that can be sent to asciidoctor")
+
+  /*
+    logDocuments a boolean specifying if documents being processed should be logged on console. Type: boolean. Default: false
+  */
+  val asciidoctorLog = SettingKey[Boolean]("Boolean flag whether to log output on the console or not (Default: false)")
 }
