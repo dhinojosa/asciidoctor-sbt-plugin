1c345ef6ac5cb3740fb39437830f973eed3ad789
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-12-22 22:12:25 -0700

    changed the names for the Keys so that there is no conflict

--- src/main/scala/com/evolutionnext/asciidoctor/sbt/plugin/AsciidoctorSbtPlugin.scala
+++ src/main/scala/com/evolutionnext/asciidoctor/sbt/plugin/AsciidoctorSbtPlugin.scala
@@ -1,6 +1,7 @@
 package com.evolutionnext.asciidoctor.sbt.plugin
 
 import sbt._
+import java.util.Collections
 
 /**
  * Copyright 2013 Daniel Hinojosa and Daniel Allen
@@ -18,13 +19,12 @@ import sbt._
  * limitations under the License.
  */
 object AsciidoctorSbtPlugin extends Plugin {
-  val asciidoctorTask = TaskKey[Unit]("asciidoctor-task")
-  val newSetting = SettingKey[String]("A new setting.")
+  val asciidoctorTaskKey = TaskKey[Unit]("asciidoctor", "Run asciidoctor")
 
   /*
    * sourceDir where the asciidoctor .asc, .asciidoc files are located, default is `src/asciidoc`
    */
-  val asciidoctorSources = SettingKey[File](
+  val asciidoctorSourcesKey = SettingKey[File](
     """Location of where the asciidoc sources with .asc,
       | .asciidoctor, .adoc, or .ad extensions (Default: src/asciidoctor)""".stripMargin)
 
@@ -32,38 +32,48 @@ object AsciidoctorSbtPlugin extends Plugin {
    * sourceDocumentName
    * an override to process a single source file. Type: File. Defaults to all files in asciidoctorSources
    */
-  val asciidoctorSingleSourceDocumentName = SettingKey[File](
+  val asciidoctorSingleSourceDocumentNameKey = SettingKey[File](
     """Location of a single .asc, .asciidoctor, .adoc, or .ad file.
-      | Type: File. Defaults to all files in %s""".format(asciidoctorSources).stripMargin)
-
+      | Type: File. Defaults to all files in %s""".format(asciidoctorSourcesKey).stripMargin)
 
   /**
    * outputDir where generated docs go. Type: File. Default: target/asciidoctor
    */
-  val asciidoctorOutputDir = SettingKey[File]("Location where the rendered documents will be stored (Default: target/asciidoctor)")
+  val asciidoctorOutputDirKey = SettingKey[File]("Location where the rendered documents will be stored (Default: target/asciidoctor)")
 
   /**
    * backends the backends to use. Type: Set<String>. Default: [html5]
    */
-  val asciidoctorBackends = SettingKey[Set[String]]("Multiple Backends used to process asciidoctor files (Default: [html5]")
+  val asciidoctorBackendsKey = SettingKey[Set[String]]("Multiple Backends used to process asciidoctor files (Default: [html5]")
 
   /**
     * options a Map specifying different options that can be sent to Asciidoctor.
     */
-  val asciidoctorOptions = SettingKey[Map[String, String]]("Options that can be sent to asciidoctor")
+  val asciidoctorOptionsKey = SettingKey[Map[String, String]]("Options that can be sent to asciidoctor")
 
   /**
     * logDocuments a boolean specifying if documents being processed should be logged on console. Type: boolean. Default: false
     */
-  val asciidoctorLog = SettingKey[Boolean]("Boolean flag whether to log output on the console or not (Default: false)")
+  val asciidoctorLogKey = SettingKey[Boolean]("Boolean flag whether to log output on the console or not (Default: false)")
 
   /**
    * default setting to be included inside of the build.sbt or Build.scala files as a sequence
    */
-  val defaultSettings = Seq(
-    asciidoctorSources := new File("src/asciidoctor"),
-    asciidoctorOutputDir := new File("target/asciidoctor"),
-    asciidoctorBackends := Set("html5"),
-    asciidoctorLog := false
+  val asciidoctorTask  = {
+    import org.asciidoctor.Asciidoctor.Factory.create
+    import org.asciidoctor.Asciidoctor._
+    import scala.collection.JavaConverters._
+    val asciidoctor = create()
+    val rendered = asciidoctor.render("*This* is it.", Map.empty[String, AnyRef].asJava)
+    println(rendered)
+  }
+
+  override lazy val settings = Seq(
+    asciidoctorSourcesKey := new File("src/asciidoctor"),
+    asciidoctorOutputDirKey := new File("target/asciidoctor"),
+    asciidoctorBackendsKey := Set("html5"),
+    asciidoctorLogKey := false,
+    asciidoctorOptionsKey := Map[String, String](),
+    asciidoctorTaskKey := asciidoctorTask
   )
 }
